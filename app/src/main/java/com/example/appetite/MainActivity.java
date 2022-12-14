95e1ea6ba51679d0ec1115fcbf1db27916c18e67
vinimichel <vinzenz.michel54@gmail.com>
2022-12-14 14:51:03 +0100

    Added Api Request for close Resaurants

--- app/src/main/java/com/example/appetite/MainActivity.java
+++ app/src/main/java/com/example/appetite/MainActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
+
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
 import com.example.appetite.adapter.NearbyViewAdapter;
 import com.example.appetite.dataModels.NearbyRestaurants;
+import com.mapbox.api.tilequery.MapboxTilequery;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView nearbyRecycler;
     NearbyViewAdapter nearbyAdapter;
+    private MapboxMap mapboxMap;     // Schnittstelle zur Map
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         setTitle("Home");
         List<NearbyRestaurants> restaurantsDataList = new ArrayList<>();
         // Platzhalterdaten hinzufügen
@@ -42,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+
     private void setRecyclerView(List<NearbyRestaurants> nearbyRecyclerList) {
         // Initialisierung des recycler der Daten über Restaurants in der nähe bereitstellt
         nearbyRecycler = findViewById(R.id.nearby_recycler);
@@ -54,5 +70,32 @@ public class MainActivity extends AppCompatActivity {
         // Recycler ruft Methoden des Adapters auf
         nearbyRecycler.setAdapter(nearbyAdapter);
 
+    }
+
+    private void buildTilequeryRequest(Point position) {
+        MapboxTilequery tilequery = MapboxTilequery.builder()
+                //.accessToken(getString(R.string.access_token))
+                .tilesetIds("mapbox.mapbox-streets-v7")
+                .query(Point.fromLngLat(position.longitude(), position.latitude()))
+                .radius(50)
+                .limit(10)
+                .geometry("polygon")
+                .dedupe(true)
+                .layers("building")
+                .build();
+        tilequery.enqueueCall(new Callback<FeatureCollection>() {
+            @Override
+            public void onResponse(Call<FeatureCollection> call, Response<FeatureCollection> response) {
+                if (response.body() != null) {
+                    FeatureCollection responseFeatureCollection = response.body();
+                    responseFeatureCollection.features();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FeatureCollection> call, Throwable throwable) {
+                Toast.makeText(MainActivity.this, "Could not retrieve data from api", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
