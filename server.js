542c2ca9d7433219069e69899dc7291f18fea7aa
Edidiong John <edwinedidiong@gmail.com>
2022-12-31 20:05:28 +0100

    Error handling #1
    HTTP error handling on server.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const createError = require('http-errors')
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
@@ -15,5 +17,19 @@ const kundenRouter = require('./routes/kunden')
 app.use('/kunden', kundenRouter)
 const restaurantRouter = require('./routes/restaurants')
 app.use('/restaurants', restaurantRouter)
+
+app.use(async (req, res, next) => {
+    const error = new Error("Not found")
+    error.status = 404
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    res.sendStatus(err.status || 500)
+    res.send({
+        status: err.status || 500,
+        message: err.message
+    })
+})
 
 app.listen(3000, () => console.log("server started"))
