78bf27e9c26bb3ab0814dbd2569750c1eadcb57f
Steven Mueller <steven.mueller@informatik.hs-fulda.de>
2023-01-12 00:40:17 +0100

    Refactoring. Translating everything bare user outputs to English, also making the code adhere to style guides to the best of my ability, e.g. removing redundant code.

--- app/src/main/java/com/example/appetite/MainActivity.java
+++ app/src/main/java/com/example/appetite/MainActivity.java
@@ -1,44 +1,33 @@
 package com.example.appetite;
 
-
 import static android.content.ContentValues.TAG;
-import static com.mapbox.turf.TurfConstants.UNIT_KILOMETERS;
-import static java.lang.Math.round;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.Activity;
 import android.content.Intent;
-
-import com.mapbox.api.geocoding.v5.models.CarmenFeature;
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
-import com.mapbox.geojson.Point;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.appetite.adapter.NearbyViewAdapter;
 import com.example.appetite.dataModels.NearbyRestaurants;
+import com.google.android.material.navigation.NavigationBarView;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 import com.mapbox.api.tilequery.MapboxTilequery;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.turf.TurfMeasurement;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import java.util.ArrayList;
+import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import com.google.android.material.navigation.NavigationBarView;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,9 +42,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Home");
         initRecyclerView();
-        // Platzhalter Startposition
+        // placeholder starting position
         Point testPoint = Point.fromLngLat(9.685242, 50.550657);
-        // tilequery bauen
         buildTilequeryRequest(testPoint);
         setBottomNavigationItem();
         initSearchFab();
@@ -65,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, SettingsActivity.class);
         startActivity(i);
     }
+
     public void launchRegister(View v) {
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
@@ -83,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.user:
+                    case R.id.home:
                         return true;
                     case R.id.find_restaurant:
                         startActivity(new Intent(getApplicationContext(), MapActivity.class));
@@ -92,8 +82,6 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.home:
-                        return true;
                 }
                 return false;
             }
@@ -101,15 +89,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setRecyclerView(List<NearbyRestaurants> nearbyRecyclerList) {
-        // Initialisierung des recycler der Daten über Restaurants in der nähe bereitstellt
+        // initialize recycler providing data about nearby restaurants
         nearbyRecycler = findViewById(R.id.nearby_recycler);
-        // Recycler soll horizontal angeordnet sein
+        // set recyclerView to horizontal
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
-        // layout Manager zuweisen
         nearbyRecycler.setLayoutManager(layoutManager);
-        // Durch Adapter werden Restaurant Daten an Views gebunden
+        // bind restaurant data to views using adapter
         nearbyAdapter = new NearbyViewAdapter(this, nearbyRecyclerList);
-        // Recycler ruft Methoden des Adapters auf
+        // call adapter methods using recycler
         nearbyRecycler.setAdapter(nearbyAdapter);
     }
 
@@ -120,66 +107,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setRecyclerViewData(List<Feature> features, Point deviceLocation) {
-        // Nearby Restaurant Daten Felder initialisieren
+        // initialize ArrayList of nearby restaurants
         List<NearbyRestaurants> restaurantsDataList = new ArrayList<>();
-        // Platzhalterdaten
+        // placeholder data
         for (Feature feature : features) {
             restaurantsDataList.add(new NearbyRestaurants(feature, deviceLocation));
         }
         nearbyAdapter = new NearbyViewAdapter(this, restaurantsDataList);
-        // Adapter auf die Daten setzen
+        // set adapter to data
         nearbyRecycler.setAdapter(nearbyAdapter);
     }
 
     private void buildTilequeryRequest(Point position) {
-        // tilequery mit tilequery api bauen
+        // build tile query with tile query API
         MapboxTilequery tilequery = MapboxTilequery.builder()
-                // Zugangstoken
-                .accessToken(MAPBOX_TOKEN)
-                // Id des Restaurant-Tileset
-                .tilesetIds("vinimichel.clcevvkko01bw23qh5gmszly4-9cpxg")
-                // Punkt von dem Abgefragt werden soll
-                .query(Point.fromLngLat(position.longitude(), position.latitude()))
-                // Wie groß darf der Radius der Restaurants maximal sein
-                .radius(10000)
-                // wie viele Ergebnisse/Restaurants sollen angezeigt werden
-                .limit(10)
-                .build();
-        // Callback bei eintreffen der Ergebnisse festlegen
+            .accessToken(MAPBOX_TOKEN)
+            .tilesetIds("vinimichel.clcevvkko01bw23qh5gmszly4-9cpxg")
+            // point of query
+            .query(Point.fromLngLat(position.longitude(), position.latitude()))
+            // MAX radius of restaurants
+            .radius(10000)
+            // how many results/restaurants are shown
+            .limit(10)
+            .build();
+        // callback on results
         tilequery.enqueueCall(new Callback<FeatureCollection>() {
             @Override
             public void onResponse(Call<FeatureCollection> call, Response<FeatureCollection> response) {
                 if (response.body() != null) {
-                    // wenn Ergebnisse gefunden setzen wir FeatureCollection
+                    // if results found, set FeatureCollection
                     FeatureCollection responseFeatureCollection = response.body();
                     List<Feature> features  = responseFeatureCollection.features();
-                    // recyclerView mit Daten logen
+                    // log recyclerView with data
                     setRecyclerViewData(features, position);
-                    Log.d(TAG, "Tilequering " + features.toString());
+                    Log.d(TAG, "Tilequering " + features);
                 }
             }
+
             @Override
             public void onFailure(Call<FeatureCollection> call, Throwable throwable) {
-                Log.d(TAG, " Tilequering: Could not retrieve data from api");
+                Log.d(TAG, "Tilequering: Could not retrieve data from API");
             }
         });
     }
 
-    // onClick Listener für Suchfeld aktivieren
+    // activate onClickListener for search
     private void initSearchFab() {
-        Mapbox.getInstance(this, MAPBOX_TOKEN); // Konfiguration des Mapbox Tokens
+        // configuration of mapbox token
+        Mapbox.getInstance(this, MAPBOX_TOKEN);
         findViewById(R.id.restaurant_search_field).setOnClickListener(new View.OnClickListener() {
-            // neue Suchaktivität öffnen wenn Button gedrückt
+            // open new search activity when button pressed
             @Override
             public void onClick(View view) {
                 Intent intent = new PlaceAutocomplete.IntentBuilder()
-                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : MAPBOX_TOKEN)
-                        .placeOptions(PlaceOptions.builder()
-                                .backgroundColor(Color.parseColor("#FFFFFF"))
-                                .limit(10)
-                                .build(PlaceOptions.MODE_CARDS))
-                        .build(MainActivity.this);
-                // Activity öffnen von der man nach dem öffnen ein Resultat wünscht welches mit onActivityResult behandelt wird
+                    .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : MAPBOX_TOKEN)
+                    .placeOptions(PlaceOptions.builder()
+                    .backgroundColor(Color.parseColor("#FFFFFF"))
+                    .limit(10)
+                    .build(PlaceOptions.MODE_CARDS))
+                    .build(MainActivity.this);
+                // open activity of which you want a result to be handled with onActivityResult
                 startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
             }
         });
@@ -189,9 +176,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
-
-            // CarmenFeature der ausgewählten Location wählen
+            // select CarmenFeature of chosen location
             CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
         }
     }
+
 }
--- app/src/main/java/com/example/appetite/MapActivity.java
+++ app/src/main/java/com/example/appetite/MapActivity.java
@@ -1,35 +1,23 @@
 package com.example.appetite;
 
-
-import static android.content.ContentValues.TAG;
-
-import android.content.Intent;
-import android.graphics.PointF;
-
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.widget.RelativeLayout;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
-import com.mapbox.geojson.Feature;
-import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
@@ -39,23 +27,29 @@ import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.location.modes.RenderMode;
-import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import java.util.List;
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import java.util.List;
+import static android.content.ContentValues.TAG;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
+public class MapActivity extends AppCompatActivity
+        implements OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
-public class MapActivity extends AppCompatActivity implements
-        OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
-
-    private MapboxMap mapboxMap;     // Schnittstelle zur Map
-    private MapView mapView; // Zugriff auf Android Mapbox SDK Methoden
-    Style style;     // Mapstyle
+    // interface to map
+    private MapboxMap mapboxMap;
+    // accessing Android Mapbox SDK methods
+    private MapView mapView;
+    // map style
+    Style style;
     private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
 
     private String geojsonSourceLayerId = "geojsonSourceLayerId";
@@ -63,18 +57,20 @@ public class MapActivity extends AppCompatActivity implements
 
     private PermissionsManager permissionsManager;
 
-    // wird benötigt um die Android SDK zu nutzen
+    // necessary to use Android SDK
     private final static String MAPBOX_TOKEN = "pk.eyJ1IjoidmluaW1pY2hlbCIsImEiOiJjbGFqdWNvYmkwZmZhM3JuMzIxYzl2Z3h4In0.bmACrWyEcA6772uD758XPw";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, MAPBOX_TOKEN); // Konfiguration des Mapbox Tokens
+        // configuring mapbox token
+        Mapbox.getInstance(this, MAPBOX_TOKEN);
         setContentView(R.layout.activity_map);
         Intent i = getIntent();
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this); // Callback Objekt wenn die Map geladen hat ist dieses Objekt
+        // callback object when map is loaded
+        mapView.getMapAsync(this);
     }
 
     public void launchReservation(View v) {
@@ -82,71 +78,62 @@ public class MapActivity extends AppCompatActivity implements
         startActivity(i);
     }
 
-
-
-    // wird aufgerufen wenn Map fertiggeladen hat
-    // legt Mapstyle fest
+    // called when map is done loading and sets map style
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-
-        // erzeugen und setzen eines Mapstyles mit URL
         mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/vinimichel/clap8w2r0002614ktc3lxzitn"), style -> onStyleLoaded(style));
     }
 
-    // wird aufgerufen wenn Style geladen ist
+    // called when style is loaded
     private void onStyleLoaded(Style style) {
         mapboxMap.addOnMapClickListener(MapActivity.this);
         this.style = style;
         initSearchFab();
 
-        // Layer Icon zur Map hinzufügen
+        // adding layer icon to map
         style.addImage(symbolIconId, BitmapFactory.decodeResource(
                 MapActivity.this.getResources(), R.drawable.blue_marker_view));
 
-        // neue leere GeoJson Quelle hinzufügen
+        // adding new, empty GeoJson source
         setUpSource(style);
 
-        // neues Layer wird in die Map eingebunden
+        // setting new layer to map
         setupLayer(style);
         enableLocationComponent(style);
     }
 
-    // Funktion zeigt Standort mithilfe des LocationComponents an
+    // shows location with LocationComponents
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-        // Check ob der Standort ermittelt werden darf, wenn nicht wird danach gefragt
+        // checks whether location can be used - if set to no, asks
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
 
-            // LocationComponent Instanz herausgreifen
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
-            // Aktiviere Standortobjekt mit gewähltem Style
+            // activate location object with chosen style
             locationComponent.activateLocationComponent(
                     LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
 
-            // Objekt sichtbar machen
             locationComponent.setLocationComponentEnabled(true);
 
-            // Kameramodus setzen
             locationComponent.setCameraMode( CameraMode.TRACKING,
-                    2500L /*duration -> Dauer des zooms*/ ,
+                    2500L /*duration of zoom*/ ,
                     13.0 /*zoom*/,
                     null /*bearing, use current/determine based on the tracking mode*/,
                     45.0 /*tilt*/,
                     null /*transition listener*/);
 
-            // Kompass als Standortsymbol
+            // compass as location symbol
             locationComponent.setRenderMode(RenderMode.COMPASS);
         } else {
-            // Um Erlaubnis zur Ermittlung des Standorts bitten
+            // ask for permission for location
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
         }
     }
 
-    /** Aufgerufen wenn User Button drückt.
-     * Rezentriet Karte auf den derzeitigen Standort des Geräts */
+    // called when user presses button, positions map to device location
     public void recenterLocation(View view) {
         if (style != null) {
             enableLocationComponent(style);
@@ -166,7 +153,6 @@ public class MapActivity extends AppCompatActivity implements
     @Override
     public void onPermissionResult(boolean granted) {
         if (granted) {
-
             mapboxMap.getStyle(style -> onStyleLoaded(style));
         } else {
             Toast.makeText(this, "Could not grant permissions", Toast.LENGTH_LONG).show();
@@ -178,71 +164,68 @@ public class MapActivity extends AppCompatActivity implements
     public boolean onMapClick(@NonNull LatLng point) {
 
         // LatLng in Bilschirmpixel umwandeln und nur  coordinates to screen pixel and only query the rendered features.
+        // I don't quite understand the above comment, signed S.M.
         final PointF pixel = mapboxMap.getProjection().toScreenLocation(point);
 
-        // gerenderte Features auf Pixel abfragen (nur Features von "fulda-restaurants"-layer
+        // query rendered features on pixels (only "fulda-restaurants" layer)
         List<Feature> features = mapboxMap.queryRenderedFeatures(pixel, "fulda-restaurants");
-        // Restaurant info Popup Layout herausgreifen
+        // grab restaurant information popup layout
         ConstraintLayout popupLayout = findViewById(R.id.place_info_layout);
 
-        // prüfen ob Restaurants an Punkt gefunden wurden
+        // checks whether restaurants have been found on coordinates
         if (features.size() > 0) {
-            // Erstes Feature aus Feature Liste herausgreifen
-            // Im Idealfall befindet sich auf einem Punkt nur ein Restaurant/Feature
             Feature feature = features.get(0);
+            // ideal case: only one restaurant/feature on coordinate
 
-            // Versichern das Feature/Restaurant Eigenschaften hat
+            // making sure restaurant/restaurant has properties
             if (feature.properties() != null) {
-                // Textfeld auf dem Namen des Restaurants zu finden ist herausgreifen
                 TextView restaurantNameField = findViewById(R.id.restaurant_name);
-                // json Eintrag mit Namen in String konvertieren und in Feld darstellen
+                // convert json entry with name to string and display as field
                 restaurantNameField.setText(feature.getProperty("title").getAsString());
                 Log.d(TAG, String.format("restaurant title = %s", feature.getProperty("title").getAsString()));
 
-                // Textfeld mit Bescreibung wählen und json Daten auf Feld ausgeben
+                // choose text field with description and print json data on field
                 TextView restaurantDescriptionField = findViewById(R.id.restaurant_description);
                 restaurantDescriptionField.setText(feature.getProperty("description").getAsString());
                 Log.d(TAG, String.format("restaurant_ description = %s", feature.getProperty("description").getAsString()));
                 
-                // falls Popup mit Informationen sichtbar machen
                 popupLayout.setVisibility(View.VISIBLE);
             } else {
-                Log.d(TAG, "Achtung das Restaurant hat keine abrufbaren Eigenschaften!");
+                Log.d(TAG, "Achtung, das Restaurant hat keine abrufbaren Eigenschaften!");
             }
         } else {
-            // falls kein Restaurant an Pixel gefunden -> Popup mit Infos verschwindent
+            // if no restaurant is found on pixels
             popupLayout.setVisibility(View.GONE);
         }
         return true;
     }
 
-
     private void setUpSource(@NonNull Style loadedMapStyle) {
         loadedMapStyle.addSource(new GeoJsonSource(geojsonSourceLayerId));
     }
 
-    // Layer hinzufügen
+    // add layer
     private void setupLayer(@NonNull Style loadedMapStyle) {
         loadedMapStyle.addLayer(new SymbolLayer("SYMBOL_LAYER_ID", geojsonSourceLayerId).withProperties(
-                iconImage(symbolIconId),
-                iconOffset(new Float[] {0f, -8f})
+            iconImage(symbolIconId),
+            iconOffset(new Float[] {0f, -8f})
         ));
     }
 
-    // onClick Listener für Suchfeld aktivieren
+    // activate onClickListener for search array
     private void initSearchFab() {
         findViewById(R.id.PLZTextfield).setOnClickListener(new View.OnClickListener() {
-            // neue Suchaktivität öffnen wenn Button gedrückt
+            // new search activity on button press
             @Override
             public void onClick(View view) {
                 Intent intent = new PlaceAutocomplete.IntentBuilder()
                         .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : MAPBOX_TOKEN)
                         .placeOptions(PlaceOptions.builder()
-                                .backgroundColor(Color.parseColor("#FFFFFF"))
-                                .limit(10)
-                                .build(PlaceOptions.MODE_CARDS))
+                        .backgroundColor(Color.parseColor("#FFFFFF"))
+                        .limit(10)
+                        .build(PlaceOptions.MODE_CARDS))
                         .build(MapActivity.this);
-                // Activity öffnen von der man nach dem öffnen ein Resultat wünscht welches mit onActivityResult behandelt wird
+                // open activity of which you want a result to be handled with onActivityResult
                 startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
             }
         });
@@ -252,41 +235,36 @@ public class MapActivity extends AppCompatActivity implements
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
-
-            // CarmenFeature der ausgewählten Location wählen
+            // select CarmenFeature of chosen location
             CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
 
-
-
-            // Neue GeoJson FeatureCollection hinzufügen und neues Feature mit oben gewählten CarmenFeature einfügen
-            // Danach springen wir zum neu hinzugefügten Feature
+            /* adding new GeoJson FeatureCollection and feature with CarmenFeature chosen above,
+            then jump to newly added feature */
             if (mapboxMap != null) {
                 Style style = mapboxMap.getStyle();
                 if (style != null) {
                     GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
                     if (source != null) {
                         source.setGeoJson(FeatureCollection.fromFeatures(
-                                new Feature[] {Feature.fromJson(selectedCarmenFeature.toJson())}));
+                            new Feature[] {Feature.fromJson(selectedCarmenFeature.toJson())}));
                     }
-                    // Textfeld des Suchfelds auf Location setzen
+                    // set text field of search field to location
                     EditText searchField = (EditText)findViewById(R.id.PLZTextfield);
-                    searchField.setText(selectedCarmenFeature.placeName(), TextView.BufferType.EDITABLE);;
+                    searchField.setText(selectedCarmenFeature.placeName(), TextView.BufferType.EDITABLE);
 
-                    // Kamera auf selektiertes Feature schweifen lassen
+                    // hover camera on selected feature
                     mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
-                            new CameraPosition.Builder()
-                                    .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
-                                            ((Point) selectedCarmenFeature.geometry()).longitude()))
-                                    .zoom(14)
-                                    .build()), 4000);
+                        new CameraPosition.Builder()
+                            .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
+                                ((Point) selectedCarmenFeature.geometry()).longitude()))
+                            .zoom(14)
+                            .build()), 4000);
                 }
             }
         }
     }
 
-
-
-    // Lifecycle Methoden der Karte
+    // lifecycle methods of map
 
     @Override
     @SuppressWarnings( {"MissingPermission"})
@@ -330,5 +308,5 @@ public class MapActivity extends AppCompatActivity implements
         super.onLowMemory();
         mapView.onLowMemory();
     }
-}
 
+}
