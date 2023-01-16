ae64ba88fd9d4fca6ceb1c23ce1c3b1ab715e9b1
Edidiong John <edwinedidiong@gmail.com>
2023-01-16 22:44:22 +0100

    API call tests
    Testing API calls in Java.

--- routes/api_call_test.java
+++ routes/api_call_test.java
@@ -1,15 +1,44 @@
+import java.io.*;
+import okhttp3.*;
 
 public class api_call_test {
 
-    public static void main(String[] args) {
+    public static void getRequest(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        try (Response response = client.newCall(request).execute()) {
+            String responseBody = response.body().string();
+            System.out.println(responseBody);
+        }
+    }
+
+    public static void postRequest(RequestBody postBody, String postUrl) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
-  .build();
-MediaType mediaType = MediaType.parse("text/plain");
-RequestBody body = RequestBody.create(mediaType, "");
-Request request = new Request.Builder()
-  .url("http://localhost:3000/reservations")
-  .method("GET", body)
-  .build();
-Response response = client.newCall(request).execute();
+                .build();
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .method("POST",postBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+
+    }
+    public static void main(String[] args) throws IOException {
+
+        String getUrl = "http://localhost:3000/users/63b347bfa3259f7f3b5b8f0c";
+        //getRequest(getUrl);
+        //post Request
+        String postUrl = "http://localhost:3000/auth/user/register";
+        MediaType mediaType = MediaType.parse("text/plain");
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, "{\n" +
+                "    \"firstname\" : \"Mohammed\",\n" +
+                "    \"lastname\" : \"Dahi\",\n" +
+                "    \"email\" : \"mdahi@gmail.com\",\n" +
+                "    \"password\" : \"kjefwhjefksb122334@@\",\n" +
+                "    \"address\" : \"Hamburgerstrasse 13\",\n" +
+                "    \"PLZ\" : \"36039\"\n" +
+                "}");
+        postRequest(body, postUrl);
     }
 }
