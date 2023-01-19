f045e9a7080000186e4527a0496e446855e7d26d
Patryk Romanowicz <rom.patryk@gmail.com>
2023-01-19 21:23:13 +0100

    add register activity for owners

---
+++ app/src/main/java/com/example/appetite/RegisterOwner.java
@@ -0,0 +1,14 @@
+package com.example.appetite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterOwner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_owner);
+    }
+}
