37ea065bf87678899f7658fe92f6c41440bbe7ee
Patryk Romanowicz <rom.patryk@gmail.com>
2023-01-19 21:55:55 +0100

    add dependency between login and register activities

--- app/src/main/java/com/example/appetite/LoginOwner.java
+++ app/src/main/java/com/example/appetite/LoginOwner.java
@@ -2,7 +2,11 @@ package com.example.appetite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginOwner extends AppCompatActivity {
 
@@ -11,4 +15,16 @@ public class LoginOwner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_owner);
     }
+
+    public void openRegisterAct(View v) {
+        TextView txt = (TextView) findViewById(R.id.signinTextOwner);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginOwner.this, RegisterOwner.class));
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/appetite/RegisterOwner.java
+++ app/src/main/java/com/example/appetite/RegisterOwner.java
@@ -2,6 +2,7 @@ package com.example.appetite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class RegisterOwner extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class RegisterOwner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_owner);
+
     }
 }
