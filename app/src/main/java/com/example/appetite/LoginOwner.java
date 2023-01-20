9f2970e0cfe9e56de9460f4d04473c825d7da394
Patryk Romanowicz <rom.patryk@gmail.com>
2023-01-20 02:51:16 +0100

    bugfix and designfix

--- app/src/main/java/com/example/appetite/LoginOwner.java
+++ app/src/main/java/com/example/appetite/LoginOwner.java
@@ -18,7 +18,7 @@ public class LoginOwner extends AppCompatActivity {
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                startActivity(new Intent(LoginOwner.this, LoginActivity.class));
+                startActivity(new Intent(LoginOwner.this, RegisterOwner.class));
             }
         });
 
--- app/src/main/java/com/example/appetite/RegisterOwner.java
+++ app/src/main/java/com/example/appetite/RegisterOwner.java
@@ -3,7 +3,10 @@ package com.example.appetite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterOwner extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class RegisterOwner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_owner);
+        TextView txt = findViewById(R.id.signInOwner);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                startActivity(new Intent(RegisterOwner.this, LoginOwner.class));
+            }
+        });
 
     }
 }
