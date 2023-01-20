7f46c35be04fff6042172916638f7fa048940440
Patryk Romanowicz <rom.patryk@gmail.com>
2023-01-20 22:30:22 +0100

    add edit and save button functionality blocking the text views

--- app/src/main/java/com/example/appetite/MyRestaurant.java
+++ app/src/main/java/com/example/appetite/MyRestaurant.java
@@ -6,16 +6,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MyRestaurant extends AppCompatActivity {
+
+    Button editButton, saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_restaurant);
         setBottomNavigationItem();
+
+        TextView[] txt = new TextView[7];
+
+        txt[0] = findViewById(R.id.editRestName);
+        txt[1] = findViewById(R.id.editDescription);
+        txt[2] = findViewById(R.id.editLongitude);
+        txt[3] = findViewById(R.id.editLatitude);
+        txt[4] = findViewById(R.id.editCity);
+        txt[5] = findViewById(R.id.editAddress);
+        txt[6] = findViewById(R.id.editZip);
+
+        editButton = findViewById(R.id.editBtn);
+        editButton.setOnClickListener(view -> {
+            for (int i = 0; i < txt.length; i++) txt[i].setEnabled(true);
+            saveButton.setEnabled(true);
+        });
+
+        saveButton = findViewById(R.id.saveBtn);
+        saveButton.setOnClickListener(view -> {
+            for (int i = 0; i < txt.length; i++) txt[i].setEnabled(false);
+            saveButton.setEnabled(false);
+            // Save the text to a database or file here
+        });
+
+        for (int i = 0; i < txt.length; i++) txt[i].setEnabled(false);
+        saveButton.setEnabled(false);
+
     }
 
     private void setBottomNavigationItem() {
